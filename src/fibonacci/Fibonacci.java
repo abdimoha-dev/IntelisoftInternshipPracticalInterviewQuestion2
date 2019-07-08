f67bb798a09b4ff6e695e8fb67c7b6c14e2d2674
Etyang Emodo Japhlet <etyangemodojaphlet@yahoo.com>
2019-07-09 00:30:38 +0300

    Write tracing recursive method that is called multiple times in the same method.

---
+++ src/fibonacci/Fibonacci.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fibonacci;
+
+/**
+ *
+ * @author Abdi Mohammed
+ */
+public class Fibonacci {
+
+    
+    public static void main(String[] args) {
+        int n = 10, t1 = 0, t2 = 1;
+        System.out.print("First " + n + " terms: ");
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.print(t1 + " + ");
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+    }
+    
+}
